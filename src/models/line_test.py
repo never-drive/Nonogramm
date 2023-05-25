171ba09777b56e649b1e052ab35c03f419b37687
Daniel Fahrni <daniel.fahrni@bzz.ch>
2023-05-25 12:17:21 +0200

    tests parameterized (reduces code duplication)

--- src/models/line_test.py
+++ src/models/line_test.py
@@ -1,3 +1,5 @@
+import pytest
+
 from models.line import Line
 
 
@@ -6,6 +8,7 @@ def test_get_dim():
     cells = sut.get_cells()
     result = sut.get_dim()
     assert result == len(cells)
+    assert not sut.is_complete()
 
 
 def test_get_value():
@@ -15,80 +18,36 @@ def test_get_value():
     assert sut.get_value(2) == '.'
 
 
-def test_get_combinations():
-    sut = Line.create(5, [5])
+@pytest.mark.parametrize("dim,blocks,expected_cell_texts", [
+    (5, [5], ['■■■■■']),
+    (5, [2, 2], ['■■✕■■']),
+    (5, [1, 2], ['✕■✕■■', '■✕✕■■', '■✕■■✕']),
+    (5, [1, 1], ['✕✕■✕■', '✕■✕✕■', '✕■✕■✕', '■✕✕✕■', '■✕✕■✕', '■✕■✕✕']),
+    (6, [1, 1, 1], ['✕■✕■✕■', '■✕✕■✕■', '■✕■✕✕■', '■✕■✕■✕'])])
+def test_get_combinations(dim: int, blocks: list[int], expected_cell_texts: list[str]):
+    sut = Line.create(dim, blocks)
     result = sut.get_combinations()
-    assert len(result) == 1
-    assert result[0] == '■■■■■'
+    assert len(result) == len(expected_cell_texts)
+    for combination in result:
+        assert combination in expected_cell_texts
 
 
-def test_get_combinations_2():
-    sut = Line.create(5, [2, 2])
-    result = sut.get_combinations()
-    assert len(result) == 1
-    assert result[0] == '■■✕■■'
-
-
-def test_get_combinations_3():
-    sut = Line.create(5, [1, 2])
-    assert len(sut.get_combinations()) == 3
-    assert '✕■✕■■' in sut.get_combinations()
-    assert '■✕✕■■' in sut.get_combinations()
-    assert '■✕■■✕' in sut.get_combinations()
-
-
-def test_get_combinations_4():
-    sut = Line.create(5, [1, 1])
-    assert len(sut.get_combinations()) == 6
-    assert '✕✕■✕■' in sut.get_combinations()
-    assert '✕■✕✕■' in sut.get_combinations()
-    assert '✕■✕■✕' in sut.get_combinations()
-    assert '■✕✕✕■' in sut.get_combinations()
-    assert '■✕✕■✕' in sut.get_combinations()
-    assert '■✕■✕✕' in sut.get_combinations()
-
-
-def test_get_combinations_5():
-    sut = Line.create(6, [1, 1, 1])
-    assert len(sut.get_combinations()) == 4
-    assert '✕■✕■✕■' in sut.get_combinations()
-    assert '■✕✕■✕■' in sut.get_combinations()
-    assert '■✕■✕✕■' in sut.get_combinations()
-    assert '■✕■✕■✕' in sut.get_combinations()
-
-
-def test_is_complete():
-    sut = Line.from_text('■■■■■', [5])
+@pytest.mark.parametrize("cell_text,blocks", [
+    ('■■■■■', [5]),
+    ('■■✕■■', [2, 2]),
+    ('■✕✕■■', [1, 2]),
+    ('■✕✕✕■', [1, 1])])
+def test_is_complete(cell_text: str, blocks: list[int]):
+    sut = Line.from_text(cell_text, blocks)
     assert sut.is_complete()
 
 
-def test_is_complete_2():
-    sut = Line.from_text('■■✕■■', [2, 2])
-    assert sut.is_complete()
-
-
-def test_is_complete_3():
-    sut = Line.from_text('■✕✕■■', [1, 2])
-    assert sut.is_complete()
-
-
-def test_is_complete_4():
-    sut = Line.from_text('■✕✕✕■', [1, 1])
-    assert sut.is_complete()
-
-
-def test_is_not_complete():
-    sut = Line.create(5, [5])
-    assert not sut.is_complete()
-
-
-def test_is_not_complete_2():
-    sut = Line.from_text('■✕✕■■', [1, 1])
-    assert not sut.is_complete()
-
-
-def test_is_not_complete_3():
-    sut = Line.from_text('■✕✕■.', [1, 1])
+@pytest.mark.parametrize("cell_text,blocks", [
+    ('■✕✕■■', [1, 1]),
+    ('■✕✕■.', [1, 1]),
+    ('■....', [1])])
+def test_is_not_complete(cell_text: str, blocks: list[int]):
+    sut = Line.from_text(cell_text, blocks)
     assert not sut.is_complete()
 
 
--- src/services/combinations_test.py
+++ src/services/combinations_test.py
@@ -1,43 +1,25 @@
+import pytest
+
 from services.combinations import Combinations
 
 
-def test_generate_pos():
-    n = 1  # Anzahl der Perlen
-    m = 2  # Anzahl der Positionen
+@pytest.mark.parametrize("perls,positions,expected_amount", [
+    (1, 2, 2),
+    (2, 2, 3),
+    (3, 2, 4),
+    (2, 3, 6)])
+def test_generate_pos(perls: int, positions: int, expected_amount: int):
+    n = perls      # Anzahl der Perlen
+    m = positions  # Anzahl der Positionen
     result = Combinations.generate_pos(n, m)
-    assert len(result) == 2
+    assert len(result) == expected_amount
 
 
-def test_generate_pos_2():
-    n = 2  # Anzahl der Perlen
-    m = 2  # Anzahl der Positionen
-    result = Combinations.generate_pos(n, m)
-    assert len(result) == 3
-
-
-def test_generate_pos_3():
-    n = 3  # Anzahl der Perlen
-    m = 2  # Anzahl der Positionen
-    result = Combinations.generate_pos(n, m)
-    assert len(result) == 4
-
-
-def test_generate_pos_4():
-    n = 2  # Anzahl der Perlen
-    m = 3  # Anzahl der Positionen
-    result = Combinations.generate_pos(n, m)
-    assert len(result) == 6
-
-
-def test_generate_gaps():
-    n = 1  # Anzahl der Perlen
-    m = 4  # Anzahl der Positionen
+@pytest.mark.parametrize("perls,positions,expected_amount", [
+    (1, 4, 4),
+    (2, 3, 6)])
+def test_generate_gaps(perls: int, positions: int, expected_amount: int):
+    n = perls      # Anzahl der Perlen
+    m = positions  # Anzahl der Positionen
     result = Combinations.generate_gaps(n, m)
-    assert len(result) == 4
-
-
-def test_generate_gaps_2():
-    n = 2  # Anzahl der Perlen
-    m = 3  # Anzahl der Positionen
-    result = Combinations.generate_gaps(n, m)
-    assert len(result) == 6
+    assert len(result) == expected_amount
