c6f3cd2fd2b42df99b9f380d675462c03bc1c5e3
Daniel Fahrni <daniel.fahrni@bzz.ch>
2023-06-01 09:23:03 +0200

    flask app added

--- backend/README.md
+++ backend/README.md
@@ -3,12 +3,19 @@
 ## Setup
 
 - `pip install -r ./requirements.txt`
+- `flask run`
 
 ## Run samples
 
 - `python nono_small.py`
 - `python nono_mouse.py`
 - `python nono.py`
+
+## Run some backend URLs
+
+- `http://127.0.0.1:5000/`
+- `http://127.0.0.1:5000/about`
+- `http://127.0.0.1:5000/capitalize/hello`
 
 ## Testing
 
---
+++ backend/app.py
@@ -0,0 +1,20 @@
+from markupsafe import escape
+from flask import Flask
+
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def hello():
+    return '<h1>Hello, World!</h1>'
+
+
+@app.route('/about/')
+def about():
+    return '<h3>This is a Flask web application.</h3>'
+
+
+@app.route('/capitalize/<word>/')
+def capitalize(word):
+    return '<h1>{}</h1>'.format(escape(word.capitalize()))
